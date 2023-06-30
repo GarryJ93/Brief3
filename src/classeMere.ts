4320177acd5d3022405338c0b5ec997229a72505
GarryJ93 <garry.joly@gmail.com>
2023-06-30 10:43:57 +0200

    Bonus 1

--- src/classeMere.ts
+++ src/classeMere.ts
@@ -1,42 +1,54 @@
 export class Hero {
-    private name: string;
-    private power: number;
-    private life: number;
-    weapon: Weapon|undefined;
+  private name: string;
+  private power: number;
+  private life: number;
+  weapon!: Weapon;
 
   constructor(name: string, power: number, life: number) {
     this.name = name;
     this.power = power;
     this.life = life;
   }
-  
-    attack(opponent: Hero) {
-         opponent.life -= this.power;
-   };
+
+  attack(opponent: Hero) {
+    if (typeof this.weapon === undefined) {
+      opponent.life -= this.power;
+    } else {
+      opponent.life -= this.power + this.weapon.damage;
+    }
+  }
   isAlive(): boolean {
     if (this.life > 0) {
       return true;
     } else {
       return false;
     }
-    }
-    getLifeHero(): number {
-        return this.life;
-    }
-    setActLife(newLife: number) {
-        this.life = newLife;
-    }
-    getPowerHero(): number {
-        return this.power;
-    }
-    setActPower(newPower: number) {
-        this.power = newPower;
-    }
-};
+  }
+  getLifeHero(): number {
+    return this.life;
+  }
+  setActLife(newLife: number) {
+    this.life = newLife;
+  }
+  getPowerHero(): number {
+    return this.power;
+  }
+  setActPower(newPower: number) {
+    this.power = newPower;
+  }
+  getNameHero(): string {
+    return this.name;
+  }
+  setActName(newName: string) {
+    this.name = newName;
+  }
+}
 
 export class Weapon {
-    name: string;
-    constructor(name: string) {
-        this.name = name;
-    }
+  name: string;
+  damage: number;
+  constructor(name: string, damage: number) {
+    this.name = name;
+    this.damage = damage;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+// Import des classes créées dans le fichier classe mère
 import { Hero } from "./classeMere";
+import { Weapon } from "./classeMere";
 
+// Création des instances de mes premières classes
 const opponent1: Hero = new Hero("Loïc", 250, 650);
 const opponent2: Hero = new Hero("Guillaume", 180, 850);
+const axe: Weapon = new Weapon("Axe", 100);
+const sword: Weapon = new Weapon("Sword", 75);
+const spear: Weapon = new Weapon("Spear", 50);
+// const spearDamage: string = Object.values(spear.damage.toString()).join("");
+// const powerUpSpear: number = Number(spearDamage);
+// const axeDamage: string = Object.values(axe.damage.toString()).join("");
+// const powerUpAxe: number = Number(spearDamage);
+// const swordDamage: string = Object.values(sword.damage.toString()).join("");
+// const powerUpSword: number = Number(spearDamage);
+
+// tests effectués lors de la création des classes et des premières instances
 
 // console.log("Base opponent1 ", opponent1);
 // console.log("Base opponent2 ", opponent2);
@@ -19,29 +33,28 @@ const opponent2: Hero = new Hero("Guillaume", 180, 850);
 // opponent2.attack(opponent1);
 // console.log("Opponent1 après attaque: ", opponent1);
 // console.log("Opponent1 alive: ", opponent1.isAlive());
+// tests effectués lors de la création des classes et des premières instances
 
-import { Weapon } from "./classeMere";
-
-const axe: Weapon = new Weapon("Axe");
-const sword: Weapon = new Weapon("Sword");
-const spear: Weapon = new Weapon("Spear");
+//Création des classes filles de Hero et initialisation de la propriété weapon
 class HeroAxe extends Hero {
   constructor(name: string, power: number, life: number) {
     super(name, power, life);
     this.weapon = axe;
   }
-
+  // polymorphism par override pour adapter la méthode à mes besoins
   attack(opponent: Hero) {
-    if(opponent instanceof HeroSpear) {
-         opponent.setActLife(opponent.getLifeHero() - this.getPowerHero()*2); 
-      } else {
-        super.attack(opponent);
-      }
-    } }
-const opponent3: HeroAxe = new HeroAxe("Garry", 540, 999);
+    if (opponent instanceof HeroSpear) {
+      opponent.setActLife(opponent.getLifeHero() - (this.getPowerHero()+ this.weapon.damage) * 2);
+    } else {
+      super.attack(opponent);
+    }
+  }
+}
+//  Création d'une instance d'une classe fille et test
+const opponent3: HeroAxe = new HeroAxe("Garry", 100, 600);
 
-console.log(opponent3);
-console.log(opponent1);
+// console.log(opponent3);
+// console.log(opponent1);
 class HeroSword extends Hero {
   constructor(name: string, power: number, life: number) {
     super(name, power, life);
@@ -50,7 +63,7 @@ class HeroSword extends Hero {
 
   attack(opponent: Hero) {
     if (opponent instanceof HeroSpear) {
-      opponent.setActLife(opponent.getLifeHero() - this.getPowerHero() * 2);
+      opponent.setActLife(opponent.getLifeHero() - (this.getPowerHero()+ this.weapon.damage) * 2);
     } else {
       super.attack(opponent);
     }
@@ -62,18 +75,19 @@ class HeroSpear extends Hero {
     this.weapon = spear;
   }
 
-    attack(opponent: Hero) {
-      if (opponent instanceof HeroSpear) {
-         opponent.setActLife(opponent.getLifeHero() - this.getPowerHero()*2); 
-      } else {
-        super.attack(opponent);
-      }
-    } 
+  attack(opponent: Hero) {
+
+    if (opponent instanceof HeroSpear) {
+      opponent.setActLife(opponent.getLifeHero() - (this.getPowerHero()+ this.weapon.damage) * 2); 
+    } else {
+      super.attack(opponent);
+    }
   }
+}
 
-
-const opponent4: HeroSpear = new HeroSpear("Marcel", 133, 666);
-const opponent5: HeroSword = new HeroSword("Antoine", 256, 166);
+// instances des autres classes filles et test
+const opponent4: HeroSpear = new HeroSpear("Marcel", 100, 1000);
+const opponent5: HeroSword = new HeroSword("Antoine", 100, 600);
 // console.log(opponent3);
 // console.log(opponent4.attack(opponent4));
 // console.log(opponent5);
@@ -81,3 +95,40 @@ const opponent5: HeroSword = new HeroSword("Antoine", 256, 166);
 // console.log(opponent4.attack(opponent5));
 // console.log(opponent5);
 
+// function contenant une boucle pour la bataille
+
+function Fight(hero1: Hero, hero2: Hero): string {
+  hero1.isAlive();
+  hero2.isAlive();
+
+  while (hero1.isAlive() && hero2.isAlive())
+    hero1.attack(hero2), hero2.attack(hero1);
+
+  if (!hero1.isAlive() && !hero2.isAlive()) {
+    console.log("it's a draw");
+    return "It's a draw !";
+  } else if (!hero1.isAlive()) {
+    console.log(`${hero2.getNameHero()} wins.`);
+    return `${hero2.getNameHero()} wins.`;
+  } else {
+    console.log(`${hero1.getNameHero()} wins.`);
+    return `${hero1.getNameHero()} wins`;
+  }
+}
+
+Fight(opponent4, opponent3);
+Fight(opponent5, opponent3);
+Fight(opponent4, opponent2);
+console.log(opponent3.getLifeHero());
+console.log(opponent4.getLifeHero());
+console.log(opponent3);
+// console.log(Object.values(axe));
+// console.log(Object.values(axe.name));
+// console.log(Object.values(spear.damage.toString()).join(""));
+
+
+// const weaponDamage: string = Object.values(spear.damage.toString()).join("");
+// console.log(weaponDamage); 
+// console.log(Number(weaponDamage));
+Fight(opponent4, opponent1);
+console.log(opponent3.weapon.damage);
